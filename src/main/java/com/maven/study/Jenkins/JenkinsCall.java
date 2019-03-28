d71d2a7ef51c9caefcca652bf75e7902649cdf7d
pjy1551 <pjy1551@gmail.com>
2019-03-28 14:55:21 +0900

    젠킨스 로그인 정보 프로퍼티 파일로 읽기

--- src/main/java/com/maven/study/Jenkins/JenkinsCall.java
+++ src/main/java/com/maven/study/Jenkins/JenkinsCall.java
@@ -1,13 +1,15 @@
 package com.maven.study.Jenkins;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.util.Properties;
+
 import org.apache.http.HttpHost;
 import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.AuthCache;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.protocol.HttpClientContext;
@@ -17,40 +19,51 @@ import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 public class JenkinsCall {
     //private static Logger logger = LoggerFactory.getLogger("");
 
     public static void main(String[] args) throws IOException {
-        String url = "";
-        String user = "";
-        String password = ""; //api token정보
-         
+        // 프로퍼티 파일 위치
+        String propFile = "C:\\Users\\pjy1551\\Desktop\\기타\\JenkinsLoginInfo.properties";
+        // 프로퍼티 객체 생성
+        Properties props = new Properties();
+        // 프로퍼티 파일 스트림에 담기
+        FileInputStream fis = new FileInputStream(propFile);
+        // 프로퍼티 파일 로딩
+        props.load(new java.io.BufferedInputStream(fis));
+        // 프로퍼티 항목 읽기
+        String url = props.getProperty("url"); // url정보
+        String user = props.getProperty("user"); // 사용자정보
+        String password = props.getProperty("password");; //api token정보
+        
+        
         String response = scrape(url, user, password);
         System.out.println(response);
 
     }
 
+    // 젠킨스 배포 정보 가져오기
     public static String scrape(String urlString, String username, String password) throws IOException {
         URI uri = URI.create(urlString);
         HttpHost host = new HttpHost(uri.getHost(), uri.getPort(), uri.getScheme());
         CredentialsProvider credsProvider = new BasicCredentialsProvider();
         credsProvider.setCredentials(new AuthScope(uri.getHost(), uri.getPort()), new UsernamePasswordCredentials(username, password));
-        // Create AuthCache instance
+        
         AuthCache authCache = new BasicAuthCache();
-        // Generate BASIC scheme object and add it to the local auth cache
+        
         BasicScheme basicAuth = new BasicScheme();
         authCache.put(host, basicAuth);
         CloseableHttpClient httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
-        HttpGet httpGet = new HttpGet(uri);
-        // Add AuthCache to the execution context
+        
+        //HttpPost httpPost = new HttpPost(uri); // post 방식 -> 배포
+        HttpGet httpGet = new HttpGet(uri); // get 방식 -> 정보조회
+       
         HttpClientContext localContext = HttpClientContext.create();
         localContext.setAuthCache(authCache);
  
-        HttpResponse response = httpClient.execute(host, httpGet, localContext);
+        HttpResponse response = httpClient.execute(host, httpGet, localContext); // httpGet, httpPost 중 선택
  
         return EntityUtils.toString(response.getEntity());
     }
